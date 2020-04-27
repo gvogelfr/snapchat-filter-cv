f1fae2766cc5789a1f8cc33d506eefca38cd6c12
Stephanie Alvarado <stephanie_alvarado@brown.edu>
2020-04-27 15:03:32 -0400

    Face detection working on Video Capture and given image

---
+++ starter.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+import dlib
+import time
+import sys
+
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+
+while True:
+    if len(sys.argv)>1:
+        img = cv2.imread(sys.argv[1])
+    else:
+        ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,     
+        scaleFactor=1.2,
+        minNeighbors=5,     
+        minSize=(20, 20)
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]  
+    cv2.imshow('faces',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
+cap.release()
+cv2.destroyAllWindows()
